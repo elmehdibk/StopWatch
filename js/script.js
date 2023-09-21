763718be862b5ecbc15ff345096a686849d6d3f0
el mehdi <elmehdibennanikhir@gmail.com>
2023-09-21 22:53:17 +0100

    create a function for stop my stopwatch

--- js/script.js
+++ js/script.js
@@ -1,50 +1,55 @@
 const seconds=document.querySelector('#S')
 const mSeconds=document.querySelector('#MS')
 const start =document.querySelector('#start')
-
+const stop =document.querySelector('#stop')
 // const date = new Date()
 
 let S =0
 let MS=0
+const timer= setInterval(()=>{
+    //MILLI SECOND
+    MS+=1
+    if(MS>=60){
+        MS=0    
+    }
+    if(MS>=10){
+        mSeconds.innerHTML=MS
+    }
+    if(MS<10){
+        mSeconds.innerHTML='0'+MS
+    }
+
+    //SECOND
+    if (MS==0) {
+        S+=1
+        setInterval(()=>{
+                if(S>=60){
+                    S=0
+                }
+                
+            },1000)
+            if(S>=10){
+                seconds.innerHTML=S+':'
+            }
+            if(S<10){
+                seconds.innerHTML='0'+S+':'
+            }
+          
+    }
+    
+    //  mSeconds.innerHTML=MS  
+},20)
+
+clearInterval(timer)
 
 start.addEventListener('click',()=>{
-    setInterval(()=>{
-        //MILLI SECOND
-        MS+=1
-        if(MS>=60){
-            MS=0    
-        }
-        if(MS>=10){
-            mSeconds.innerHTML=MS
-        }
-        if(MS<10){
-            mSeconds.innerHTML='0'+MS
-        }
-    
-        //SECOND
-        if (MS==0) {
-            S+=1
-            setInterval(()=>{
-                    if(S>=60){
-                        S=0
-                    }
-                    
-                },1000)
-                if(S>=10){
-                    seconds.innerHTML=S+':'
-                }
-                if(S<10){
-                    seconds.innerHTML='0'+S+':'
-                }
-              
-        }
-        
-        //  mSeconds.innerHTML=MS  
-    },20)
+   console.log(timer); 
     
 })
 
-
+stop.addEventListener('click',()=>{
+    clearInterval(timer)
+})
 
         
  
