862e8946901100c1d42d09f1572146f072bd6455
el mehdi <elmehdibennanikhir@gmail.com>
2023-09-21 01:22:08 +0100

    create functions for display milli second and second

--- js/script.js
+++ js/script.js
@@ -1,17 +1,47 @@
-const sw=document.querySelector('#stop-watch')
+const seconds=document.querySelector('#S')
+const mSeconds=document.querySelector('#MS')
 
-const date = new Date()
+// const date = new Date()
 
-let S = date.getSeconds()
-
+let S =0
+let MS=0
 
 
 setInterval(()=>{
-    S+=1
-    if(S>60){
-        S=0
+    //MILLI SECOND
+    MS+=1
+    if(MS>=60){
+        MS=0    
     }
-    sw.innerHTML=S; 
-  },1000)
+    if(MS>=10){
+        mSeconds.innerHTML=MS
+    }
+    if(MS<10){
+        mSeconds.innerHTML='0'+MS
+    }
 
-// console.log(setS);
+    //SECOND
+    if (MS==0) {
+        S+=1
+        setInterval(()=>{
+                if(S>=60){
+                    S=0
+                }
+                
+            },1000)
+            if(S>=10){
+                seconds.innerHTML=S
+            }
+            if(S<10){
+                seconds.innerHTML='0'+S
+            }
+          
+    }
+    
+    //  mSeconds.innerHTML=MS  
+},20)
+
+
+        
+ 
+    
