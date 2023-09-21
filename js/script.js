9307bcc077ca798499aabe7ae1febbd116c8ca42
el mehdi <elmehdibennanikhir@gmail.com>
2023-09-21 22:20:39 +0100

    create a function for start my stopWatch

--- js/script.js
+++ js/script.js
@@ -1,45 +1,49 @@
 const seconds=document.querySelector('#S')
 const mSeconds=document.querySelector('#MS')
+const start =document.querySelector('#start')
 
 // const date = new Date()
 
 let S =0
 let MS=0
 
-
-setInterval(()=>{
-    //MILLI SECOND
-    MS+=1
-    if(MS>=60){
-        MS=0    
-    }
-    if(MS>=10){
-        mSeconds.innerHTML=MS
-    }
-    if(MS<10){
-        mSeconds.innerHTML='0'+MS
-    }
-
-    //SECOND
-    if (MS==0) {
-        S+=1
-        setInterval(()=>{
-                if(S>=60){
-                    S=0
-                }
-                
-            },1000)
-            if(S>=10){
-                seconds.innerHTML=S+':'
-            }
-            if(S<10){
-                seconds.innerHTML='0'+S+':'
-            }
-          
-    }
+start.addEventListener('click',()=>{
+    setInterval(()=>{
+        //MILLI SECOND
+        MS+=1
+        if(MS>=60){
+            MS=0    
+        }
+        if(MS>=10){
+            mSeconds.innerHTML=MS
+        }
+        if(MS<10){
+            mSeconds.innerHTML='0'+MS
+        }
     
-    //  mSeconds.innerHTML=MS  
-},20)
+        //SECOND
+        if (MS==0) {
+            S+=1
+            setInterval(()=>{
+                    if(S>=60){
+                        S=0
+                    }
+                    
+                },1000)
+                if(S>=10){
+                    seconds.innerHTML=S+':'
+                }
+                if(S<10){
+                    seconds.innerHTML='0'+S+':'
+                }
+              
+        }
+        
+        //  mSeconds.innerHTML=MS  
+    },20)
+    
+})
+
 
 
         
